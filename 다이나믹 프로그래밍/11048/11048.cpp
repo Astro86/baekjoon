5786177deed1f9fa7f5dfc9a386a68712c738bf7
양동우 <ckck803@naver.com>
2020-03-06 13:25:47 +0900

    이동하기

---
+++ 다이나믹 프로그래밍/11048/11048.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[1010][1010];
+int dp[1010][1010];
+
+int main(void)
+{
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    dp[0][0] = arr[0][0];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            int y = i;
+            int x = j;
+            int ny = y + 1;
+            int nx = x + 1;
+
+            if (0 <= ny && ny < N) {
+                dp[ny][x] = max(dp[ny][x], dp[y][x] + arr[ny][x]);
+            }
+
+            if (0 <= nx && nx < M) {
+                dp[y][nx] = max(dp[y][nx], dp[y][x] + arr[y][nx]);
+            }
+
+            if (0 <= ny && ny < N && 0 <= nx && nx < M) {
+                dp[ny][nx] = max(dp[ny][nx], dp[y][x] + arr[ny][nx]);
+            }
+        }
+    }
+
+    cout << dp[N - 1][M - 1] << '\n';
+
+    return 0;
+}
