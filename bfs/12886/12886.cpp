27b617f99e9987c15feec99a345f377a9c2d9c88
양동우 <ckck803@naver.com>
2020-03-06 22:14:57 +0900

    돌 그룹

---
+++ bfs/12886/12886.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <queue>
+#include <set>
+using namespace std;
+
+int a, b, c;
+set<pair<pair<int, int>, int>> check;
+
+int main(void) {
+    cin >> a >> b >> c;
+
+    queue<pair<pair<int, int>, int>> q;
+    q.push({{a, b}, c});
+    check.insert({{a, b}, c});
+
+    while (!q.empty()) {
+        int cntA = q.front().first.first;
+        int cntB = q.front().first.second;
+        int cntC = q.front().second;
+        q.pop();
+
+        if (cntA == cntB && cntB == cntC) {
+            cout << 1 << '\n';
+            return 0;
+        }
+
+        if (cntA != cntB) {
+            int nextA, nextB;
+            if (cntA > cntB) {
+                nextA = cntA - cntB;
+                nextB = cntB + cntB;
+
+            } else {
+                nextA = cntA + cntA;
+                nextB = cntB - cntA;
+            }
+            if (check.find({{nextA, nextB}, cntC}) == check.end()) {
+                check.insert({{nextA, nextB}, cntC});
+                q.push({{nextA, nextB}, cntC});
+            }
+        }
+        if (cntA != cntC) {
+            int nextA, nextC;
+            if (cntA > cntC) {
+                nextA = cntA - cntC;
+                nextC = cntC + cntC;
+            } else {
+                nextA = cntA + cntA;
+                nextC = cntC - cntA;
+            }
+
+            if (check.find({{nextA, cntB}, nextC}) == check.end()) {
+                check.insert({{nextA, cntB}, nextC});
+                q.push({{nextA, cntB}, nextC});
+            }
+        }
+        if (cntB != cntC) {
+            int nextB, nextC;
+            if (cntB > cntC) {
+                nextB = cntB - cntC;
+                nextC = cntC + cntC;
+            } else {
+                nextB = cntB + cntB;
+                nextC = cntC - cntB;
+            }
+
+            if (check.find({{cntA, nextB}, nextC}) == check.end()) {
+                check.insert({{cntA, nextB}, nextC});
+                q.push({{cntA, nextB}, nextC});
+            }
+        }
+    }
+
+    cout << 0 << endl;
+    return 0;
+}
