7b49ab50f068227e92e34144d266d67d87a4a013
양동우 <ckck803@naver.com>
2020-03-06 13:25:00 +0900

    파도반 수열

---
+++ 다이나믹 프로그래밍/9461/9461.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int T;
+int N;
+
+int main(void)
+{
+    cin >> T;
+
+    while (T--) {
+        cin >> N;
+        vector<long long> v;
+
+        v.push_back(1);
+        v.push_back(1);
+        v.push_back(1);
+
+        for (int i = 0; i < N; i++) {
+            long long temp;
+            temp = v[i] + v[i + 1];
+            v.push_back(temp);
+
+            if (v.size() == N) {
+                break;
+            }
+        }
+
+        cout << v[N - 1] << '\n';
+    }
+
+    return 0;
+}
