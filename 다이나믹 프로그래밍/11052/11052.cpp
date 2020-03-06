021a4c5b79f908d0a1b6bd19c2ff1005187a7d74
양동우 <ckck803@naver.com>
2020-03-06 13:26:12 +0900

    카드 구매하기

---
+++ 다이나믹 프로그래밍/11052/11052.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int arr[10001];
+int money[10001];
+
+int main(void)
+{
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+
+    money[1] = arr[1];
+    for (int i = 2; i <= N; i++) {
+        for (int j = 1; j <= N && j <= i; j++) {
+            money[i] = max(money[i], money[i - j] + arr[j]);
+        }
+    }
+
+    cout << money[N] << '\n';
+
+    return 0;
+}
