40c1250b1669ac1d7ccb53f49b953e4dc0493d1b
양동우 <ckck803@naver.com>
2020-03-06 21:37:07 +0900

    A+B - 3

---
+++ 수학/10950/10950.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int T;
+int a, b;
+
+int main(void){
+    cin>>T;
+
+    while(T--){
+        cin>>a>>b;
+
+        cout<<a+b<<'\n';
+    }
+
+    return 0;
+}
