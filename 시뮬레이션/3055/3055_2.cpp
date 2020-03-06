1ea4beb057a0847771308116a3598a009bbbe81d
양동우 <ckck803@naver.com>
2020-03-06 21:13:58 +0900

    탈출

---
+++ 시뮬레이션/3055/3055_2.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int R, C;
+char map[51][51];
+int waterMap[51][51];
+bool check[51][51];
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+queue<pair<int, int>> point;
+queue<pair<int, int>> water;
+
+void flood()
+{
+    while (!water.empty()) {
+        int y = water.front().first;
+        int x = water.front().second;
+        water.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (0 <= ny && ny < R && 0 <= nx && nx < C) {
+                if (map[ny][nx] == '.') {
+                    if (waterMap[ny][nx] > waterMap[y][x] + 1) {
+                        waterMap[ny][nx] = waterMap[y][x] + 1;
+                        water.push({ ny, nx });
+                    }
+                }
+            }
+        }
+    }
+}
+
+void findD()
+{
+    int level = 0;
+    int point_size;
+    while (!point.empty()) {
+        level++;
+        point_size = point.size();
+
+        while (point_size--) {
+            int y = point.front().first;
+            int x = point.front().second;
+            point.pop();
+
+            if (map[y][x] == 'D') {
+                cout << level - 1 << '\n';
+                return;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if (0 <= ny && ny < R && 0 <= nx && nx < C) {
+                    if (map[ny][nx] != 'X' && check[ny][nx] == false && level < waterMap[ny][nx]) {
+                        check[ny][nx] = true;
+                        point.push({ ny, nx });
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "KAKTUS" << '\n';
+    return;
+}
+
+int main(void)
+{
+    cin >> R >> C;
+
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> map[i][j];
+            waterMap[i][j] = 10000;
+
+            if (map[i][j] == 'S') {
+                point.push({ i, j });
+                check[i][j] = true;
+            }
+
+            if (map[i][j] == '*') {
+                water.push({ i, j });
+                waterMap[i][j] = 0;
+            }
+        }
+    }
+
+    flood();
+    findD();
+
+    return 0;
+}
---
+++ 시뮬레이션/3055/tempCodeRunnerFile.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int R, C;
+char map[51][51];
+bool check[51][51];
+queue<pair<int, int>> water;
+queue<pair<int, int>> q;
+
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+void flood(int n)
+{
+    int size = n;
+
+    while (size--) {
+        int y = water.front().first;
+        int x = water.front().second;
+        water.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (0 <= ny && ny < R && 0 <= nx && nx < C) {
+                if (map[ny][nx] == '.') {
+                    map[ny][nx] = '*';
+                    water.push({ ny, nx });
+                }
+            }
+        }
+    }
+}
+
+bool findS(int n)
+{
+    int size = n;
+
+    while (size--) {
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (0 <= ny && ny < R && 0 <= nx && nx < C) {
+                if (map[ny][nx] == '.' && check[ny][nx] == false) {
+                    bool isPass = true;
+                    for (int j = 0; j < 4; j++) {
+                        int nny = ny + dy[j];
+                        int nnx = nx + dx[j];
+
+                        if (map[nny][nnx] == '*') {
+                            isPass = false;
+                        }
+                    }
+                    if (isPass == true) {
+                        check[ny][nx] = true;
+                        q.push({ ny, nx });
+                    }
+                } else if (map[ny][nx] == 'D') {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+int main(void)
+{
+    cin >> R >> C;
+
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> map[i][j];
+
+            if (map[i][j] == 'S') {
+                check[i][j] = true;
+                q.push({ i, j });
+            }
+
+            if (map[i][j] == '*') {
+                water.push({ i, j });
+            }
+        }
+    }
+
+    int q_size = q.size();
+    int water_size = water.size();
+    int level = 0;
+    bool isPass = false;
+    while (q_size != 0 && water_size != 0) {
+
+        isPass = findS(q_size);
+        level++;
+        if (isPass == true) {
+            break;
+        }
+
+        flood(water_size);
+
+        water_size = water.size();
+        q_size = q.size();
+    }
+
+    if (isPass == true) {
+        cout << level << '\n';
+    } else {
+        cout << "KAKTUS" << '\n';
+    }
+
+    return 0;
+}
