24a5cc98ca143d1704ac2fd2829aac335e709e85
양동우 <ckck803@naver.com>
2020-03-06 13:26:01 +0900

    이항 계수

---
+++ 다이나믹 프로그래밍/11051/tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int N, K;
+int dp[1001][1001];
+
+int main(void)
+{
+    cin >> N >> K;
+
+    dp[0][0] = 1;
+
+    for (int i = 1; i <= N; i++) {
+        dp[i][0] = 1;
+        dp[i][i] = 1;
+
+        for (int j = 0; j < i; j++) {
+            dp[i + 1][j + 1] = dp[i][j] + dp[i][j + 1] % 10007;
+        }
+    }
+
+    cout << dp[N][K] << '\n';
+
+    return 0;
+}
