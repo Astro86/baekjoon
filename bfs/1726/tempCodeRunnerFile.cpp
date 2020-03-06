cd6216cd1529a01a8def65a09029fb94fefb4548
양동우 <ckck803@naver.com>
2020-03-06 21:45:54 +0900

    로봇

---
+++ bfs/1726/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+        for (int i = 1; i <= M; i++) {
+            for (int j = 1; j <= N; j++) {
+                cout << check[i][j][2] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
