ecef99c1ea5d75da88b82262a50ff1c973d7d914
양동우 <ckck803@naver.com>
2020-03-06 21:12:04 +0900

    문자열

---
+++ 시뮬레이션/1120/1120.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string A, B;
+int minDiff = 51;
+
+int main(void)
+{
+    cin >> A >> B;
+
+    int ASize = A.size();
+    int BSize = B.size();
+
+    for (int i = 0; i <= BSize - ASize; i++) {
+        int diff = 0;
+        for (int j = 0; j < ASize; j++) {
+            if (A[j] != B[i + j]) {
+                diff++;
+            }
+        }
+
+        if (minDiff > diff) {
+            minDiff = diff;
+        }
+    }
+
+    cout << minDiff << '\n';
+
+    return 0;
+}
