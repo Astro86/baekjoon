1f0b2099dddbc30f31d75889b58ce064e9e8b4e0
양동우 <ckck803@naver.com>
2020-03-06 21:12:23 +0900

    프린터 큐

---
+++ 시뮬레이션/1966/1966.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int testcase;
+int n, m;
+vector<int> important;
+int maxNum;
+int totalNum;
+
+int main(void)
+{
+    cin >> testcase;
+
+    while (testcase--) {
+        cin >> n >> m;
+        queue<pair<int, int>> q;
+        totalNum = 0;
+        maxNum = 0;
+        important = vector<int>(10);
+
+        for (int i = 0; i < n; i++) {
+            int number;
+            cin >> number;
+
+            q.push({ number, i });
+            important[number] += 1;
+
+            if (maxNum < number) {
+                maxNum = number;
+            }
+        }
+
+        while (!q.empty()) {
+            int cntPriority = q.front().first;
+            int cntIndex = q.front().second;
+            q.pop();
+
+            if (cntPriority < maxNum) {
+                q.push({ cntPriority, cntIndex });
+                continue;
+            }
+
+            totalNum++;
+
+            if (cntIndex == m) {
+                cout << totalNum << '\n';
+                break;
+            }
+
+            important[maxNum] -= 1;
+            while (important[maxNum] == 0) {
+                maxNum--;
+            }
+        }
+    }
+}
