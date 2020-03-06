8556cce5400adb224976ae0f2ee3311f0e3b3ed0
양동우 <ckck803@naver.com>
2020-03-06 13:28:58 +0900

    가장 긴 증가하는 부분 수열2

---
+++ 다이나믹 프로그래밍/12015/12015.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MAX 1000001
+
+int N;
+
+int main(void)
+{
+    cin >> N;
+    int arr[MAX];
+    int cache[MAX];
+
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+
+    cache[1] = arr[1];
+    int length = 1;
+    for (int i = 2; i <= N; i++) {
+        int cntValue = arr[i];
+
+        if (cache[length] < cntValue) {
+            length += 1;
+            cache[length] = cntValue;
+        } else {
+            int index = lower_bound(cache + 1, cache + length + 1, cntValue) - cache;
+            cache[index] = cntValue;
+        }
+    }
+
+    cout << length << '\n';
+    return 0;
+}
