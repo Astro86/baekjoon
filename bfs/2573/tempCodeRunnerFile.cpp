32b0a8ab951578e65ae1d813aabf16a05b339cdd
양동우 <ckck803@naver.com>
2020-03-06 21:51:27 +0900

    빙산

---
+++ bfs/2573/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+        // for (int j = 0; j < n; j++) {
+        //     for (int k = 0; k < m; k++) {
+        //         cout << check[j][k] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        // cout << endl
+        //      << endl;
