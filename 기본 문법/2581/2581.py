02beb1101ff154914049b32e64bda977cb7f83bf
양동우 <ckck803@naver.com>
2020-05-09 01:28:52 +0900

    소수

---
+++ 기본 문법/2581/2581.py
@@ -0,0 +1,21 @@
+check = [False]*10001
+
+for i in range(2, 10001):
+    if(i*i > 10000):
+        break
+    for j in range(2*i, 10001, i):
+        check[j] = True
+
+l = []
+sum = 0
+n = int(input())
+m = int(input())
+for i in range(n, m+1):
+    if(i > 1 and check[i] == False):
+        l.append(i)
+        sum += i
+if(len(l) != 0):
+    print(sum)
+    print(min(l))
+else:
+    print(-1)
