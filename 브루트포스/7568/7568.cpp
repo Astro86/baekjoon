b11b72c4583c0e037143baf7aa38a549cef9bf7f
양동우 <ckck803@naver.com>
2020-05-12 13:06:50 +0900

    덩치

--- 브루트포스/7568/7568.cpp
+++ 브루트포스/7568/7568.cpp
@@ -6,26 +6,21 @@ vector<pair<int, int>> wh_list;
 vector<int> score;
 int n;
 
-int main(void)
-{
+int main(void) {
     scanf("%d", &n);
     wh_list = vector<pair<int, int>>(n, make_pair(0, 0));
     score = vector<int>(n, 1);
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         scanf("%d %d", &wh_list[i].first, &wh_list[i].second);
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             if (wh_list[i].first < wh_list[j].first && wh_list[i].second < wh_list[j].second)
                 score[i]++;
         }
     }
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         printf("%d ", score[i]);
     }
     return 0;
